8ed5ded9ee4ca219fa42b0984eb0c3b761a6b592
denilabo <denilaboski@gmail.com>
2016-05-03 17:54:13 +0300

    Added files via upload

---
+++ Integers.java
@@ -0,0 +1,16 @@
+package Integers;
+
+import java.util.Arrays;
+
+
+public class Integers {
+	public static void main (String[] args){
+	
+		int []  Integers= {1,2,3,4,5};
+	
+	
+		    for (int i = 0; i <= 1; i++)
+		       System.out.println(Integers[i]);
+			
+		}
+	}
