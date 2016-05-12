7b83b30cd92c02cf3156d13c0a8b1f9d02240fc0
denilabo <denilaboski@gmail.com>
2016-05-12 17:54:48 +0300

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,56 +1,26 @@
-package rectangle;
+package shape;
 
+public class Rectangle1 extends Shape {
+	private double height;
+	private double width;
 
-public class Rectangle {
-private double height;
-private double width;
-private String color; 
-public Rectangle(double wid, double high){
-height = high;
-width = wid;
-} 
-public Rectangle(){
-height = 1;
-width = 1;
-color = "White";
-} 
-public void setHeight(double high){
-height = high;
-}
-public void setWidth(double wid){
-width = wid;
-}
-public void setColor(String col){
-color = col;
-} 
-public double getArea(){
-return height*width;
-}
-public double getPerimeter(){
-return 2*(height + width);
-}
-public void getColor(){
-System.out.println("Color is: " + color +"\n");
-}
+	public Rectangle1(double wid, double high) {
+		height = high;
+		width = wid;
+	}
 
-public static void main(String[] args){
-Rectangle rectangle1 = new Rectangle();
-Rectangle rectangle2 = new Rectangle(4, 40);
-Rectangle rectangle3 = new Rectangle(3.5, 35.9);
+	public Rectangle1() {
+		height = 1;
+		width = 1;
 
+	}
 
-rectangle2.setColor("red");
-rectangle3.setColor("blue");
-rectangle1.getColor();
-rectangle2.getColor();
-rectangle3.getColor();
-System.out.println("The perimeter of the first rectangle is: " + rectangle1.getPerimeter() + "\n");
-System.out.println("The perimeter of the second rectangle is: " + rectangle2.getPerimeter() + "\n");
-System.out.println("The perimeter of the third rectangle is: " + rectangle3.getPerimeter() + "\n");
-System.out.println("The area of the first rectangle is: " + rectangle1.getArea() + "\n");
-System.out.println("The area of the second rectangle is: " + rectangle2.getArea() + "\n");
-System.out.println("The area of the third rectangle is: " + rectangle3.getArea() + "\n");
+	public double getArea() {
+		return height * width;
+	}
+
+	public double getPerimeter() {
+		return 2 * (height + width);
+	}
+
 }
-}
-
-
